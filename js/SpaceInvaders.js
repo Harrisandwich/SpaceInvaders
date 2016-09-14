6ddab249a58f23acb690fbaaf2ffe5950f0b5e7c
Harrison Hutcheon <harrison.hutcheon@threepointturn.com>
2016-09-14 18:21:39 -0400

    base updated

--- js/SpaceInvaders.js
+++ js/SpaceInvaders.js
@@ -1,30 +1,42 @@
+//pixi aliases
+var Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite,
+    TextureCache = PIXI.utils.TextureCache,
+    Container = PIXI.Container;
+
+//
+var renderer = new autoDetectRenderer(256, 256);
+var stage = new Container();
+var state = null;
+
+var enemies = [];
+var player = null;
+
+function gameLoop()
+{
+    requestAnimationFrame(gameLoop);
+
+    state();
+
+    renderer.render(stage);
+}
+
+function play() {
+    
+    
+}
+
+function mainMenu() { 
+    
+        
+}
+
 $(document).ready(function(){
 
-    var renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight);
-    var rgb2hex = PIXI.utils.rgb2hex;
-    var stage = new PIXI.Container();
-    var state = null;
-
     document.body.appendChild(renderer.view);
-    gameLoop();
-    
-   
-    function gameLoop()
-    {
-        requestAnimationFrame(gameLoop);
-
-        state();
-
-        renderer.render(stage);
-    }
-    function play() {
-        
-        
-    }
-
-    function menu() { 
-        
-          
-    }
+    //gameLoop();
     
 });
